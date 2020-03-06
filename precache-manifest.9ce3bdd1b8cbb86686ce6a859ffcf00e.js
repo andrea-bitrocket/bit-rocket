f0d06257e4e6ae25d1ab3bac3594a9a58cc5859d
andrea-bitrocket <andrea@bitrocket.dev>
2020-03-06 12:03:30 +0100

    Updates

---
+++ precache-manifest.9ce3bdd1b8cbb86686ce6a859ffcf00e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd68e656827de39cce78bead200a6cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c639e18be1ec8db070c",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3a67640e2d473dd7c1bc",
+    "url": "/static/css/main.4bbd96e9.chunk.css"
+  },
+  {
+    "revision": "4c639e18be1ec8db070c",
+    "url": "/static/js/2.fcfefd23.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.fcfefd23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a67640e2d473dd7c1bc",
+    "url": "/static/js/main.fb8503c5.chunk.js"
+  },
+  {
+    "revision": "3dcd0946067c60964471",
+    "url": "/static/js/runtime-main.b261bd92.js"
+  },
+  {
+    "revision": "73e6697756652f69fc9cd5af01a39c84",
+    "url": "/static/media/Axiforma-Regular.73e66977.eot"
+  },
+  {
+    "revision": "b4afda971855a50bfaaf56e6cd6b1f85",
+    "url": "/static/media/Axiforma-Regular.b4afda97.woff"
+  },
+  {
+    "revision": "d882de2cc508ece40b39e18c754dd9f3",
+    "url": "/static/media/Axiforma-Regular.d882de2c.ttf"
+  }
+]);
